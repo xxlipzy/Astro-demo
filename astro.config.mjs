87712c1a90ff37dcb2a715df059e05a59a4798ba
Alexander Thompson <xxlipzy@gmail.com>
2026-07-03 11:02:09 +0100

    Tailwind Install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,10 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
